3030449e80f5156c9cb416b5809f1f95b7654ac3
luoquan1982 <luo.quan1982@gmail.com>
2020-04-29 21:52:11 +0800

    完成稀疏矩阵到原始矩阵以及原始矩阵转换成稀疏矩阵的代码逻辑

--- src/sparseMatrix/SparseMatrix.java
+++ src/sparseMatrix/SparseMatrix.java
@@ -1,5 +1,12 @@
 package sparseMatrix;
 
+import tool.ArrayTool;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * SparseMatrix
  *
@@ -7,6 +14,61 @@ package sparseMatrix;
  * @date 2020/4/25 20:19
  */
 public class SparseMatrix {
+
+    /**
+     * 普通数组转稀疏矩阵
+     *
+     * @param twoDimensionalArray
+     * @return
+     */
+    public static int[][] toSparseArray(int[][] twoDimensionalArray) {
+        int row = twoDimensionalArray.length;
+        int column = twoDimensionalArray[0].length;
+
+        List<Map<String,Integer>> data = new LinkedList<>();
+
+        for (int r = 0; r < twoDimensionalArray.length; r++) {
+            for (int c = 0; c < twoDimensionalArray[r].length; c++) {
+                if (0 != twoDimensionalArray[r][c]) {
+                    Map<String, Integer> e = new HashMap<>();
+                    e.put("row",r);
+                    e.put("column",c);
+                    e.put("value",twoDimensionalArray[r][c]);
+                    data.add(e);
+                }
+            }
+        }
+
+        int[][] ret = new int[data.size() + 1][3];
+        ret[0][0] = row;
+        ret[0][1] = column;
+        ret[0][2] = data.size();
+
+        int current = 1;
+
+        for(Map<String,Integer> e:data){
+            ret[current][0] = e.get("row");
+            ret[current][1] = e.get("column");
+            ret[current][2] = e.get("value");
+            current++;
+        }
+        return ret;
+    }
+
+    /**
+     * 稀疏矩阵转换回原始数组
+     * @param spareArray
+     * @return
+     */
+    public static int[][] toOriginalArray(int[][] spareArray){
+        int[][] ret = new int[spareArray[0][1]][spareArray[0][1]];
+        int number = spareArray[0][2];
+        for(int i = 1; i <= number; i++){
+            ret[spareArray[i][0]][spareArray[i][1]] = spareArray[i][2];
+        }
+        return ret;
+    }
+
     public static void main(String[] args) {
         // 创建一个原始的二维数组11*11
         // 0:表示没有旗子,1:表示黑子,2:表示白子
@@ -15,55 +77,17 @@ public class SparseMatrix {
         chessArray1[2][3] = 2;
         //输出原始的二维数组
         System.out.println("原始的二维数组");
-        printArray(chessArray1);
-
-        //将二维数组转稀疏数组的思路
-        //1.先遍历二维数组得到非零数据的个数
-        int sum = 0;
-        for (int[] row : chessArray1) {
-            for (int item : row) {
-                if (0 != item) {
-                    sum++;
-                }
-            }
-        }
-
-        //创建对应的稀疏数组
-        int[][] sparseArray = new int[sum + 1][3];
-        //给稀疏数组赋值
-        sparseArray[0][0] = chessArray1.length;
-        sparseArray[0][1] = chessArray1[0].length;
-        sparseArray[0][2] = sum;
-
-        //遍历二维数组,将非零的值存放到稀疏数组中
-        int current = 1;
-        for(int r=0;r<chessArray1.length;r++){
-            for(int c=0;c<chessArray1[r].length;c++){
-                if(0!= chessArray1[r][c]){
-                    sparseArray[current][0] = r;
-                    sparseArray[current][1] = c;
-                    sparseArray[current][2] = chessArray1[r][c];
-                    current++;
-                }
-            }
-        }
-
+        ArrayTool.prinTwoDimensionalArray(chessArray1);
         System.out.println();
-        System.out.println("转换的稀疏数组为:");
-        printArray(sparseArray);
 
-        int[][] randomArray = new int[4][4];
+        System.out.println("原始数组转换成稀疏矩阵");
+        int[][] ret = toSparseArray(chessArray1);
+        ArrayTool.prinTwoDimensionalArray(ret);
         System.out.println();
-        System.out.println("Random Array:");
-        printArray(randomArray);
+
+        System.out.println("稀疏矩阵转换成原始数组");
+        int[][] originArray = toOriginalArray(ret);
+        ArrayTool.prinTwoDimensionalArray(originArray);
     }
 
-    public static void printArray(int[][] array){
-        for(int[] row:array){
-            for(int item:row){
-                System.out.printf("%d\t",item);
-            }
-            System.out.println();
-        }
-    }
 }
---
+++ src/tool/ArrayTool.java
@@ -0,0 +1,19 @@
+package tool;
+
+/**
+ * ArrayTool
+ *
+ * @author LuoQuan
+ * @date 2020/4/27 19:45
+ */
+public class ArrayTool {
+
+    public static void prinTwoDimensionalArray(int[][] array){
+        for(int[] row:array){
+            for(int element:row){
+                System.out.printf("%d\t",element);
+            }
+            System.out.println();
+        }
+    }
+}
